3b17346418c75e75807e8ce46b9835847bfd50ed
Lyubomir Nachev <48567009+LyubomirNachev@users.noreply.github.com>
2022-06-24 00:53:40 +0300

    Add files via upload

--- audio_test.ino
+++ audio_test.ino
@@ -1,19 +1,19 @@
 #include <TMRpcm.h>
 #include <SPI.h>
-#include <SD.h>
+#include <SdFat.h>
 #define CHIP_SELECT_PIN  10
 #define SPEAKER_PIN      9
 TMRpcm tmrpcm;
-
+SdFat sd;
 void setup( ) {
     Serial.begin( 9600 );
   tmrpcm.speakerPin = SPEAKER_PIN;
   
   while ( !Serial ) /*mt*/ ;
-  if ( !SD.begin( CHIP_SELECT_PIN ) )
+  if ( !sd.begin( CHIP_SELECT_PIN ) )
     Serial.println( "SD fail" );
   else {
-    tmrpcm.play( "2.WAV" );
+    tmrpcm.play( "MBT Polov Jivot.wav" );
     delay( 5 );
   }
 }
